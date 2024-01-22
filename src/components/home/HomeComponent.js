62861b03a57b18cd270f62844ee945b28ab25685
Ajinkya Suryawanshi <ajinkyasuryawanshi20@gmail.com>
2024-01-23 01:24:21 +0530

    Update contact anchor tag attributes and developer summary

--- src/components/home/HomeComponent.js
+++ src/components/home/HomeComponent.js
@@ -13,10 +13,10 @@ function HomeComponent() {
           <section className="homenav-social">
             <nav>
               <ul>
-                <li><a href="https://www.linkedin.com/in/ajinkya-suryawanshi-815851190/"><img src="/linkedin_icon.png" alt="linkedIn"/></a></li>
-                <li><a href="https://github.com/ajinkya2020"><img src="/github_icon.png" alt="github"/></a></li>
-                <li><a href="https://www.instagram.com/_aajinkya_/"><img src="/instagram_icon.png" alt="instagram"/></a></li>
-                <li><a href="mailto: ajinkyasuryawanshi20@gmail.com"><img src="/mail_icon.png" alt="gmail"/></a></li>
+                <li><a href="https://www.linkedin.com/in/ajinkya-suryawanshi-815851190/" target="_blank" rel='noopener noreferrer'><img src="/linkedin_icon.png" alt="linkedIn"/></a></li>
+                <li><a href="https://github.com/ajinkya2020" target="_blank" rel='noopener noreferrer'><img src="/github_icon.png" alt="github"/></a></li>
+                <li><a href="https://www.instagram.com/_aajinkya_/" target="_blank" rel='noopener noreferrer'><img src="/instagram_icon.png" alt="instagram"/></a></li>
+                <li><a href="mailto: ajinkyasuryawanshi20@gmail.com" target="_blank" rel='noopener noreferrer'><img src="/mail_icon.png" alt="gmail"/></a></li>
               </ul>
             </nav>
           </section>
@@ -24,7 +24,7 @@ function HomeComponent() {
         <div className="text-left">
           <div className="font-bold text-yellow-600 introduction-title">INTRODUCTION</div>
           <div>Full Stack Web Developer</div>
-          <div className="text-gray-400">I am a software developer based off in India.<br></br> My tech stack include Angular, React, Mongodb, MySQL, Express, Node</div>
+          <div className="text-gray-400">I work as a software developer based in India,<br></br> specializing in Angular, React, MongoDB, MySQL, Express, and Node for my tech stack.</div>
         </div>
       </div>
     </div>
