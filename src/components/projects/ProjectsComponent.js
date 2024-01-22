565b2c4c835f612737d977563e8796b6c977451b
Ajinkya Suryawanshi <ajinkyasuryawanshi20@gmail.com>
2024-01-23 01:24:57 +0530

    Update projects component to add preview and source code links

--- src/components/projects/ProjectsComponent.js
+++ src/components/projects/ProjectsComponent.js
@@ -3,29 +3,46 @@ import './ProjectsComponent.css'
 
 let projectList = [
   {
-    title: "Social Scoop",
-    image: './socialScoop.png',
-    desc: 'Skilled in Angular, React, Node.js, and Express.js. Expertise in crafting dynamic and scalable web solutions. Proven track record in building robust SPAs and efficient UIs.'
-  },
-  {
-    title: "Covid 19 Tracker",
+    title: "COVID-19 Tracker",
     image: './covid19Tracker.png',
-    desc: 'Versatile in Unity Engine, Skilled in creating captivating gaming experiences, implementing gameplay mechanics'
+    desc: "A comprehensive COVID-19 Tracker, meticulously designed to provide real-time updates on global and country-specific cases. Stay informed on cases, recoveries, and losses.",
+    skills: ['Javascript', 'React', 'disease.sh'],
+    projectLink: "https://covid19tracker-e2bcf.web.app/",
+    sourceCodeLink: "https://github.com/ajinkya2020/Covid-19-Tracker"
   },
   {
     title: "Capsule Wars",
     image: './capsuleWars.png',
-    desc: 'Versatile UI/UX designer with a keen eye for creating visually appealing and user-centric digital experiences.'
+    desc: "Dive into the action-packed world of Capsule Wars, an adrenaline-fueled FPS game where players, embodying capsule-shaped warriors fight to win. Engage in fast-paced multiplayer battles, strategize, and emerge victorious.",
+    skills: ['C#', 'Unity Engine', 'Photon'],
+    sourceCodeLink: "https://github.com/ajinkya2020/Capsule-Wars"
   },
+  {
+    title: "ng-godspeed-transfer (npm package)",
+    image: './ngGodspeedTransfer.png',
+    desc: 'Angular component for effortless item transfer between two lists, optimizing your workflow with seamless and intuitive functionality.',
+    skills: ['Typescript', 'Angular'],
+    projectLink: "https://www.npmjs.com/package/ng-godspeed-transfer",
+    sourceCodeLink: "https://github.com/ajinkya2020/ng-godspeed"
+  },
+  {
+    title: "The Social Scoop",
+    image: './socialScoop.png',
+    desc: 'Social Scoop is a unique social media platform that celebrates the beauty of real-life moments.',
+    skills: ['Javascript', 'React', 'Firebase'],
+    projectLink: "https://social-scoop.web.app/",
+    sourceCodeLink: "https://github.com/ajinkya2020/The-Social-Scoop"
+  }
 ]
 
 function ProjectsComponent() {
   return (
     <div className='projects-container mt-20 py-5'>
       <div className="font-bold text-yellow-600 introduction-title">FEATURED PROJECTS</div>
+      <div>Explore my projects and witness the fusion of technology and genius.</div>
       <div className='flex flex-wrap justify-center'>
         {projectList.map((skill, index) => (
-          <ProjectCardComponent key={index} cardImage={skill.image} title={skill.title} desc={skill.desc}></ProjectCardComponent>
+          <ProjectCardComponent key={index} cardImage={skill.image} title={skill.title} desc={skill.desc} skills={skill.skills} projectLink={skill.projectLink} sourceCodeLink={skill.sourceCodeLink}></ProjectCardComponent>
         ))}
       </div>
     </div>
@@ -34,11 +51,35 @@ function ProjectsComponent() {
 
 function ProjectCardComponent(props) {
   return (
-    <div className='card-container m-5 block p-20'>
-      <img className='projectImage' src={props.cardImage} alt="linkedIn"/>
-      <div className='mt-3 font-bold'>{props.title}</div>
-      <div className='mt-3 text-gray-400'>{props.desc}</div>
+    <div className='project-card-container m-10'>
+      <div className="flip-card-inner">
+        <div className="flip-card-front">
+          <div>
+            <img className='projectImage' src={props.cardImage} alt="linkedIn"/>
+            <div className='mt-3 font-bold'>{props.title}</div>
+            <div className='mt-3'>
+              {props.skills.map((skill, index) => (
+                <ProjectSkill key={index} skill={skill}></ProjectSkill>
+              ))}
+            </div>
+          </div>
+          <div className='mt-3 text-gray-400'>{props.desc}</div>
+        </div>
+        <div className="flip-card-back py-40 px-28">
+          <div>
+            <div className='font-bold'>{props.title}</div>
+            <button className={!props.projectLink ? 'mt-10 cursor-not-allowed' : 'mt-10'}><a href={props.projectLink} target="_blank" rel='noopener noreferrer'>Live Preview</a></button>
+            <button className={!props.sourceCodeLink ? 'mt-5 cursor-not-allowed' : 'mt-5'}><a href={props.sourceCodeLink} target="_blank" rel='noopener noreferrer'>Source Code</a></button>
+          </div>
+        </div>
+      </div>
     </div>
+  )
+}
+
+function ProjectSkill(props) {
+  return (
+    <span className='bg-white text-black text-sm rounded-xl py-1 px-2 ms-3'>{props.skill}</span>
   )
 }
 
