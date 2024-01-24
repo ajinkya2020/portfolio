86334812aa7cd1a8defceaa4b3b8a7332ff745d0
Ajinkya Suryawanshi <ajinkyasuryawanshi20@gmail.com>
2024-01-25 01:07:26 +0530

    add navbar item active effect

--- src/components/header/HeaderComponent.js
+++ src/components/header/HeaderComponent.js
@@ -1,22 +1,34 @@
-import React from 'react'
+import { React, useState} from 'react'
 import { Link } from "react-router-dom";
 import './HeaderComponent.css'
 
+const listItems = [
+  { title: "Home", route: '/'},
+  { title: "Projects", route: 'projects'},
+  { title: "Contact", route: 'contact'},
+];
+
 function HeaderComponent() {
+  const [activeItem, setActiveItem] = useState(0);
+
+  const handleItemClick = (index) => {
+    setActiveItem(index);
+  };
+
   return (
     <div className='header-main flex justify-between px-5 py-3 sticky top-0 z-10'>
       <span className='text-3xl'>AJ<span className='font-bold text-yellow-600'>.</span></span>
       <nav>
         <ul className='flex item-list text-l py-2'>
-          <li>
-            <Link to={`/`}>Home</Link>
-          </li>
-          <li>
-            <Link to={`projects`}>Projects</Link>
-          </li>
-          <li>
-            <Link to={`contact`}>Contact</Link>
-          </li>
+          {listItems.map((item, index) => (
+            <li
+              key={index}
+              className={index === activeItem ? 'active px-2' : 'px-2'}
+              onClick={() => handleItemClick(index)}
+            >
+              <Link to={item.route}>{item.title}</Link>
+            </li>
+          ))}
         </ul>
       </nav>
     </div>
