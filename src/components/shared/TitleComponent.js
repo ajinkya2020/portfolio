685f62122f2801195dfad967c5987e629d7664e3
Ajinkya Suryawanshi <ajinkyasuryawanshi20@gmail.com>
2024-01-28 02:06:18 +0530

    Add TitleComponent

---
+++ src/components/shared/TitleComponent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TitleComponent(props) {
+  return (
+    <div className="font-bold text-xs text-yellow-600">{props.title}</div>
+  )
+}
+
+export default TitleComponent
