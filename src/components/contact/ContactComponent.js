57d59bcf670cd8d2fd2eab2910a25c754bc3145c
Ajinkya Suryawanshi <ajinkyasuryawanshi20@gmail.com>
2024-02-05 01:02:09 +0530

    add contact form

--- src/components/contact/ContactComponent.js
+++ src/components/contact/ContactComponent.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import TitleComponent from '../shared/TitleComponent'
+import './ContactComponent.css'
 
 function ContactComponent() {
   return (
-    <div>ContactComponent</div>
+    <div className='mt-20'>
+      <TitleComponent title='CONTACT'></TitleComponent>
+      <div>Lets talk about ideas</div>
+      <form className='contact-form mt-10 mx-auto'>
+        <div className='flex-col'>
+          <div className="relative">
+            <input type="text" className="block rounded-t-lg rounded-b-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-gray-50 dark:bg-gray-600 appearance-none dark:text-white dark:border-gray-600 focus:outline-none focus:ring-0 peer" placeholder=" " />
+            <label className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Name</label>
+          </div>
+          <div className="relative mt-5">
+            <input type="text" className="block rounded-t-lg rounded-b-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-gray-50 dark:bg-gray-600 appearance-none dark:text-white dark:border-gray-600 focus:outline-none focus:ring-0 peer" placeholder=" " />
+            <label className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Email</label>
+          </div>
+          <div className="relative mt-5">
+            <textarea type="text" rows={10} className="block rounded-t-lg rounded-b-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-gray-50 dark:bg-gray-600 appearance-none dark:text-white dark:border-gray-600 focus:outline-none focus:ring-0 peer" placeholder=" "></textarea>
+            <label className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Message</label>
+          </div>
+          <button className='h-14 w-40 mt-5'>Submit</button>
+        </div>
+      </form>
+    </div>
   )
 }
 
